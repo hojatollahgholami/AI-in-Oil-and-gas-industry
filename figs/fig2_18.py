09db17fcdfd45a902c1261fdbcc4d09f7b73a2fa
hojatollahgholami <64473418+hojatollahgholami@users.noreply.github.com>
2025-07-24 12:14:28 +0330

    Update fig2_18.py

--- figs/fig2_18.py
+++ figs/fig2_18.py
@@ -84,7 +84,7 @@ axs[1].grid(alpha=0.2)
 
 # ایجاد بارکد
 qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=5, border=2)
-qr.add_data("https://B2n.ir/hr4292")
+qr.add_data("https://B2n.ir/mm4460")
 qr.make(fit=True)
 
 # تبدیل بارکد به تصویر
