a4dfb4f1425c84858c6f80a3dc3ce9b1ef937551
hojatollahgholami <64473418+hojatollahgholami@users.noreply.github.com>
2025-07-24 08:57:02 +0330

    Update fig2_17.py

--- figs/fig2_17.py
+++ figs/fig2_17.py
@@ -168,7 +168,7 @@ for bar in bars:
 
 # ایجاد بارکد
 qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=5, border=2)
-qr.add_data("https://B2n.ir/df5225")
+qr.add_data("https://B2n.ir/hr4292")
 qr.make(fit=True)
 
 # تبدیل بارکد به تصویر
